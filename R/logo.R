a42fe03b5dd247bf3ace16812135bdb67ce2e666
David Hodge <david.hodge@stats.govt.nz>
2021-04-09 13:03:47 +1200

    Update

--- R/logo.R
+++ R/logo.R
@@ -3,58 +3,29 @@
 # library(simplevis)
 # 
 # nz <- simplevis::nz %>%
-#   mutate(dummy = 1) %>%
+#   mutate(dummy = "1") %>%
 #   sf::st_as_sf()
-# 
-# ggplot(nz) +
-#   geom_sf() +
-#   scale_y_continuous(limits = c(-48, -34), expand = c(0, 0)) +
-#   scale_x_continuous(limits = c(166, 180), expand = c(0, 0))
 # 
 # plot <- ggplot_sf_col(nz, dummy, title = "",
 #                       pal = "transparent",
-#                       legend_labels = "") +
-#   theme(panel.background = element_rect(fill = "#deebf7")) +
-#   scale_y_continuous(limits = c(-48, -34), expand = c(0, 0)) +
-#   scale_x_continuous(limits = c(166, 180), expand = c(0, 0))
+#                       col_labels = "") +
+#   theme(panel.background = element_rect(fill = "#EAEFF3")) +
+#   theme(panel.grid = element_blank())
 # 
 # plot
 # 
-# sticker(plot, package="simplevis",
-#         p_color = "#08519c",
-#         h_color = "#08519c",
-#         p_size=8, p_family = "sans serif", p_y = 1.0,
+# sticker(plot, 
+#         package="simplevis",
+#         p_color = simplevis:::pal_default(1),
+#         h_color = simplevis:::pal_default(1),
+#         h_fill = "#C1D1DD",
+#         p_size=25, 
+#         p_family = "Helvetica", 
+#         p_y = 1.0,
 #         p_x = 1.0,
-#         s_x=1.0, s_y=0.89, s_width = 2.5, s_height=3.5,
-#         filename="inst/figures/logo.png", white_around_sticker = TRUE)
-
-# library(tidyverse)
-# library(hexSticker)
-# library(simplevis)
-# 
-# nz <- simplevis::nz %>%
-#   mutate(dummy = 1) %>%
-#   sf::st_as_sf()
-# 
-# ggplot(nz) +
-#   geom_sf() +
-#   scale_y_continuous(limits = c(-48, -34), expand = c(0, 0)) +
-#   scale_x_continuous(limits = c(166, 180), expand = c(0, 0))
-# 
-# plot <- ggplot_sf_col(nz, dummy, title = "",
-#                       pal = "transparent",
-#                       legend_labels = "") +
-#   theme(panel.background = element_rect(fill = "#c1d1dd")) +
-#   scale_y_continuous(limits = c(-48, -34), expand = c(0, 0)) +
-#   scale_x_continuous(limits = c(166, 180), expand = c(0, 0))
-# 
-# plot
-# 
-# sticker(plot, package="simplevis",
-#         p_color = "#31688e",
-#         h_color = "#31688e",
-#         p_size=8, p_family = "sans serif", p_y = 1.0,
-#         p_x = 1.0,
-#         s_x=1.0, s_y=0.89, s_width = 2.5, s_height=3.5,
-#         filename="inst/figures/logo.png", white_around_sticker = TRUE)
-
+#         s_x=0.44, 
+#         s_y=0.9, 
+#         s_width = 10, 
+#         s_height= 15,
+#         filename="logo.png", 
+#         white_around_sticker = TRUE)

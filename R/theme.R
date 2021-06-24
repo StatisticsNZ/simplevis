f6a590d0894e09d87246eb8b53255102eb0773f9
Your Name <david.hodge@stats.govt.nz>
2021-06-25 10:16:18 +1200

    Update

--- R/theme.R
+++ R/theme.R
@@ -218,7 +218,7 @@ theme_x_gridlines <-
     )
   }
 
-#' @title Theme for point ggplots.
+#' @title Theme for graphs with x and y gridlines.
 #' @param font_family Font family to use. Defaults to "".
 #' @param font_size_title Font size for the title text. Defaults to 11.
 #' @param font_size_body Font size for all text other than the title. Defaults to 10.
